fa763a2fbae3c9be9d068b5b98c9a64bac3d5cd4
Stephen Coleman <stcolema@tcd.ie>
2024-02-24 17:26:36 -0800

    Remove salso::salso as a means of obtaining a point estimate clustering for unsupervised methods. Change to S. Wade's minVI method (implemented here to avoid dependency on a non-CRAN package)

--- R/predictFromMultipleChains.R
+++ R/predictFromMultipleChains.R
@@ -86,7 +86,6 @@
 #' )
 #' ensemble_mod <- predictFromMultipleChains(mcmc_outputs, burn)
 #' }
-#' @importFrom salso salso
 #' @export
 predictFromMultipleChains <- function(mcmc_outputs,
                                       burn,
@@ -190,8 +189,8 @@ predictFromMultipleChains <- function(mcmc_outputs,
     merged_outputs$prob <- .prob <- apply(.alloc_prob, 1, max)
     merged_outputs$pred <- apply(.alloc_prob, 1, which.max)
   } else {
-    merged_outputs$pred <- suppressWarnings(salso::salso(merged_outputs$samples))
+    merged_outputs$psm <- .psm <- createSimilarityMat(merged_outputs$samples)
+    merged_outputs$pred <- minVI(.psm, merged_outputs$samples, method = "avg") # suppressWarnings(salso::salso(merged_outputs$samples))
   }
-
   merged_outputs
 }
